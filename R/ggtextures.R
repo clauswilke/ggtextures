5f1823eb10535540cb20ee50216f74f010579589
Claus Wilke <wilke@austin.utexas.edu>
2018-09-25 23:16:00 -0500

    initial package setup

---
+++ R/ggtextures.R
@@ -0,0 +1,9 @@
+#' Tools for drawing textured rectangles and bars with ggplot
+#'
+#' @name ggtextures
+#' @docType package
+#' @import ggplot2
+#' @import rlang
+#' @import grid
+#' @importFrom purrr pmap
+NULL
